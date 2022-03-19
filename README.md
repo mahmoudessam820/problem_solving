f5049eb156b8006f3d44031b2c44bb454ab981aa
mahmoud essam <esame4166@gmail.com>
2022-03-19 18:36:53 +0200

    'first'

---
+++ README.md
@@ -0,0 +1,3 @@
+# Problems Solving
+
+Problem solving is the act of defining a problem; determining the cause of the problem; identifying, prioritizing, and selecting alternatives for a solution; and implementing a solution.
---
+++ python/codewars.com/task_10.py
@@ -0,0 +1,26 @@
+'''
+    Simple Encryption #1 - Alternating Split
+
+    - Implement a pseudo-encryption algorithm which given a string S and an integer N
+    - concatenates all the odd-indexed characters of S with all the even-indexed characters of S
+    - this process should be repeated N times.
+
+    Examples:
+
+    encrypt("012345", 1)  =>  "135024"
+    encrypt("012345", 2)  =>  "135024"  ->  "304152"
+    encrypt("012345", 3)  =>  "135024"  ->  "304152"  ->  "012345"
+
+    Together with the encryption function, you should also implement a decryption function which reverses the process.
+    If the string S is an empty value or the integer N is not positive, return the first argument without changes.
+
+    Task URL: https://www.codewars.com/kata/simple-encryption-number-1-alternating-split
+
+'''
+
+def decrypt(encrypted_text, n):
+    pass
+
+
+def encrypt(text, n):
+    pass
